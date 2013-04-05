1d3b5793b8e8a4c681d88bf15bbf412c43b900a0
AnnKpx Server <annkpx@gmail.com>
2013-04-05 14:47:26 +0400

    from server

--- apps/rates/models.py
+++ apps/rates/models.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-from ckeditor.fields import RichTextField
-
 class Item(models.Model):
     name = models.CharField(max_length=300, verbose_name=u'заголовок')
     header_photo = models.FileField(upload_to= 'uploads/headers', max_length=256, blank=True, verbose_name=u'шапка')
     header_photo_min = models.FileField(upload_to= 'uploads/mini_headers', max_length=256, blank=True, verbose_name=u'шапка меньше размером')
     image = models.FileField(upload_to= 'uploads/photos', max_length=256, blank=True, verbose_name=u'фотография')
-    rules = RichTextField(blank=True, verbose_name=u'правила')
+    rules = models.TextField(blank=True, verbose_name=u'правила')
     show = models.BooleanField(blank=True, default=True, verbose_name=u'показывать на сайте?')
     sort_parameter = models.IntegerField(blank=True, default=0, verbose_name=u'параметр для сортировки')
     class Meta:
